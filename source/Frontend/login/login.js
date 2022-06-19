d9c6f3be9f2bbc8a59496dac3d3e896f9e1635fd
birkanTonkal <tonkalkamilbirkan@gmail.com>
2022-06-19 18:15:20 +0300

    login style changed

--- source/Frontend/login/login.js
+++ source/Frontend/login/login.js
@@ -1,22 +1,59 @@
-
+import {guidGenerator} from '../helpers/guidGenerator.js'
+console.log("sadsadsadsa")
 let count = 1
 let url = "http://localhost:5102/api/User"
-let loginButton = document.getElementById("login")
+const signUpButton = document.getElementById('submitRegister');
+const signInButton = document.getElementById('submitLogin');
+const container = document.getElementById('container');
+const singIn = document.getElementById('signIn');
+const singUp = document.getElementById('signUp');
 
+singUp.addEventListener('click', () => {
+  container.classList.add("right-panel-active");
+});
 
-loginButton.addEventListener("click", async () => {
-    let email = document.getElementById("email")
-    let password = document.getElementById("password").value
+singIn.addEventListener('click', () => {
+  container.classList.remove("right-panel-active");
+});
+
+signInButton.addEventListener("click", async (e) => {
+    let email = document.getElementById("loginEmail").value
+    let password = document.getElementById("loginPassword").value
     let response = await fetch(url)
     let allUsers = await response.json()
     for (const iterator of allUsers) {
-       window.location.href = "/Frontend/index.html"
-       if (iterator.password == password && iterator.email == email) {
+      console.log(email)
+       console.log(password)
+      if (iterator.password == password && iterator.email == email) {
         console.log(window.location.href)
+        window.location.href = "../index.html"
        }
     }
 })
-
+signUpButton.addEventListener('click', async (e) => {
+  let userKey = guidGenerator();
+    e.preventDefault();
+    let email = document.getElementById("registerEmail").value
+    let password = document.getElementById("registerPassword").value
+    let user = {
+        "rowState": 12,
+        "usersKey": userKey,
+        "userType": "normal",
+        "email": email,
+        "password": password,
+    }
+    let send = await fetch(url, {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(user)
+    }).then((response) => {
+        console.log(response)
+        if (response.status == 200){window.location.href = "../login/login.html"}
+    })
+});
 const element = document.querySelector('form');
 element.addEventListener('submit', event => {
   event.preventDefault();
--- source/Frontend/product/product.js
+++ source/Frontend/product/product.js
@@ -1,6 +1,9 @@
+import {guidGenerator} from '../helpers/guidGenerator.js'
+console.log("sas")
 let addProduct = document.getElementById("addProduct");
 let url = "http://localhost:5102/api/Product";
-addProduct.addEventListener("click", () => {
+addProduct.addEventListener("click", async () => {
+    let userKey = guidGenerator();
     let title = document.getElementById("title").value;
     let price = document.getElementById("price").value;
     let graphicCard = document.getElementById("graphicCard").value;
@@ -8,14 +11,14 @@ addProduct.addEventListener("click", () => {
     let cpu = document.getElementById("cpu").value;
     let ram = document.getElementById("ram").value;
     let stock = document.getElementById("stock").value;
-
-    let data = {
+    console.log(stock)
+    let product = {
         "rowState": 1,
-        "productKey": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+        "productKey":userKey,
         "title": title,
         "price": price,
         "type": type,
-        "dateCreated": "2022-06-19T13:06:46.782Z",
+        /* "dateCreated": "2022-06-19T13:06:46.782Z", */
         "graphicCard": graphicCard,
         "cpu": cpu,
         "ram": ram,
@@ -23,6 +26,16 @@ addProduct.addEventListener("click", () => {
         "stock": stock
     }
 
-    
+    let send = await fetch(url, {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(product)
+    }).then((response) => {
+        console.log(response)
+        if (response.status == 200){console.log("sa")}
+    })
 
 })
