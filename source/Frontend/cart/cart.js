594521ac6466399f48d175ae5fa78da571f69066
birkanTonkal <tonkalkamilbirkan@gmail.com>
2022-06-20 21:40:45 +0300

    delete product feature added

--- source/Frontend/cart/cart.js
+++ source/Frontend/cart/cart.js
@@ -1,9 +1,12 @@
 let url = "http://localhost:5102/api/Cart"
+
 let getProduct = "/api/Product/{id}"
 let products = document.getElementById("product")
 async function getAllCarts () {
     let response = await fetch(url);
+   
     let allProducts = await response.json();
+    console.log(allProducts)
     let price = 0;
     let newPrice = 1; 
    
@@ -14,10 +17,25 @@ async function getAllCarts () {
         let productJson = await getProduct.json()
         var product = document.createElement("div");
         product.className = "product";
-        product.innerHTML = `<div class="content"><div><h1>${productJson.title}</h1><h2>${productJson.price}</h2></div><div class ="counter"><select name="counter" id="counter"><option value="one">1</option><option value="two">2</option><option value="three">3</option><option value="four">4</option><option value="five">5</option></select></div><div class="cop"><img src='../trash-bin.png'></div></div>`;
+        product.innerHTML = `<div class="content"><div><h1>${productJson.title}</h1><h2>${productJson.price}</h2></div><div class ="counter"><select name="counter" id="counter"><option value="one">1</option><option value="two">2</option><option value="three">3</option><option value="four">4</option><option value="five">5</option></select></div><div class="cop"><img class="deleteCart" src='../trash-bin.png'></div></div>`;
         price += productJson.price;
         products.appendChild(product);
+        document.querySelectorAll(".deleteCart").forEach(element => {
+            
+            element.addEventListener("click", async () => {
+                let userId = sessionStorage.getItem("usersKey")
+            userId = userId.toString()
+            let deleteUrl = `http://localhost:5102/api/Cart/${userId}` 
+                let response = await fetch(deleteUrl,{
+                    method: "DELETE",
+                    headers: {"Content-Type":"application/json"}
+                });
+                window.location.href = window.location.href;
+            })
+        })
     } 
+
+    
     
     
     var priceDiv = document.createElement("div");
--- source/Frontend/index.js
+++ source/Frontend/index.js
@@ -6,75 +6,94 @@ let products = document.getElementById("products");
 let addProduct = document.getElementById("addProduct");
 let cart = document.getElementById("cart");
 if (sessionStorage.getItem("userType") == "normal") {
-    addProduct.style.display = "none" 
-}else {
-    cart.style.display = "none"
+    addProduct.style.display = "none";
+} else {
+    cart.style.display = "none";
 }
 goCart.addEventListener("click", () => {
-    window.location.href = "./cart/cart.html"
-}) 
+    window.location.href = "./cart/cart.html";
+});
 /*
 
 */
 
 addProduct.addEventListener("click", () => {
-    window.location.href = "./product/product.html"
-})
+    window.location.href = "./product/product.html";
+});
 async function getAllProducts() {
     let response = await fetch(url);
     allProducts = await response.json();
     console.log(allProducts);
-    let i=0;
-    const images = ["casper.png", "devices.png", "devices.png","devices.png","devices.png"];
+    let i = 0;
+    const images = [
+        "casper.png",
+        "devices.png",
+        "devices.png",
+        "devices.png",
+        "devices.png",
+    ];
     if (sessionStorage.getItem("userType") == "normal") {
         for (const iterator of allProducts) {
             var container = document.createElement("div");
             container.className = "container";
-            container.innerHTML = `<div class="container page-wrapper"><div class="page-inner"><div class="row"><div class="el-wrapper"><div class="box-up"><img class="img" src=${images[i]} ;alt="photo"><div class="img-info"><div class="info-inner"><span class="p-name">${iterator.title}</span><p class="productKey" style="display:none"><span class="p-company">${iterator.type}</span></div><div class="a-size">Features:<span class="size">${iterator.ram}</span></div></div></div><div class="box-down"><div class="h-bg"><div class="h-bg-inner"></div></div><a class="cart" href="#"><span class="price">${iterator.price}₺</span><button class="addCart"><span class="txt">Add in cart</span></button></a></div></div></div></div></div>`
-            products.appendChild(container); 
+            container.innerHTML = `<div class="container page-wrapper"><div class="page-inner"><div class="row"><div class="el-wrapper"><div class="box-up"><img class="img" src=${images[i]} ;alt="photo"><div class="img-info"><div class="info-inner"><span class="p-name">${iterator.title}</span><p class="productKey" style="display:none"><span class="p-company">${iterator.type}</span></div><div class="a-size">Features:<span class="size">${iterator.ram}</span></div></div></div><div class="box-down"><div class="h-bg"><div class="h-bg-inner"></div></div><a class="cart" href="#"><span class="price">${iterator.price}₺</span><button id=${iterator.productKey} class="addCart"><span class="txt">Add in cart</span></button></a></div></div></div></div></div>`;
+            products.appendChild(container);
             i++;
-    }
-    }else {
-       
-        products.innerHTML='<h1>ALL PRODUCTS</h1><br><table><tr><th width="14.28%">Title</th><th width="14.28%">Price</th><th width="14.28%">Type</th><th width="14.28%">Ram</th><th width="14.28%">Graphic Card</th><th width="14.28%">CPU</th><th width="14.28%" >Stock</th></tr></table>'
+        }
+    } else {
+        products.innerHTML =
+            '<h1>ALL PRODUCTS</h1><br><table><tr><th width="14.28%">Title</th><th width="14.28%">Price</th><th width="14.28%">Type</th><th width="14.28%">Ram</th><th width="14.28%">Graphic Card</th><th width="14.28%">CPU</th><th width="14.28%" >Stock</th></tr></table>';
 
         for (const iterator of allProducts) {
             var product = document.createElement("div");
             product.className = "product";
             product.innerHTML = `<table><tr><td>${iterator.title}</td><td>${iterator.price}</td><td>${iterator.type}</td><td>${iterator.ram}</td><td>${iterator.graphicCard}</td><td>${iterator.cpu}</td><td>${iterator.stock}</td></tr></table>
-            <p class="productKey" style="display:none">${iterator.productKey}</p>`;
+            <p class="productKey" style="display:none">${iterator.productKey}</p><button id=${iterator.productKey} class="delete">delete</button>`;
             products.appendChild(product);
-          
         }
+        let deleteButton = document.querySelectorAll(".delete");
+        deleteButton.forEach((element) => {
+            element.addEventListener("click", async () => {
+                let key = element.id.toString();
+                let cartUrl = `http://localhost:5102/api/Cart/${key}`;
+                let productUrl = "http://localhost:5102/api/Product/" + key;
+                console.log(productUrl);
+                window.location.href = productUrl;
+                let a = await fetch(productUrl, {
+                    method: "DELETE",
+                    headers: { "Content-Type": "application/json" },
+                });
+                window.location.href = window.location.href;
+            });
+        });
     }
 
-    let addCarts = document.querySelectorAll(".addCart")
+    let addCarts = document.querySelectorAll(".addCart");
     let div_array = [...addCarts];
-    div_array.forEach(element =>  {
-        console.log(element)
+    div_array.forEach((element) => {
+        console.log(element);
         element.addEventListener("click", async (e) => {
-            let userKey = sessionStorage.getItem("usersKey")
-            console.log(userKey)
-            let productKey = e.target.previousElementSibling.innerHTML
-            console.log(productKey)
+            let userKey = sessionStorage.getItem("usersKey");
+            userKey = userKey.toString()
+            let productKey = element.id;
+            productKey = productKey.toString()
             let cartObject = {
-                "rowState": 2,
-                "userKey": userKey,
-                "productKey": productKey
-              };
-              await fetch(cartUrl, {
-                method: 'POST',
+                rowState: 2,
+                userKey: userKey,
+                productKey: productKey,
+            };
+            await fetch(cartUrl, {
+                method: "POST",
                 headers: {
                     'Accept': 'application/json',
                     'Content-Type': 'application/json'
                 },
-                body: JSON.stringify(cartObject)}).then(e => {
-                    console.log(e)
-            
-        })
-        })
-    })
-    
+                body: JSON.stringify(cartObject),
+            }).then((e) => {
+                console.log(e);
+            });
+        });
+    });
 }
 
 getAllProducts();
